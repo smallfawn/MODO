0e526693c635696993a82719238cfbdba7967f5e
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-12-05 19:28:20 +0800

    Update bncr_lufly.js

--- bncr_lufly.js
+++ bncr_lufly.js
@@ -2,7 +2,7 @@
  * @author smallfawn
  * @name bncr_lufly
  * @team smallfawn
- * @version 1.0.0
+ * @version 1.0.1
  * @description luflyV3
  * @rule ^(京东登录)
  * @admin false
@@ -48,28 +48,82 @@ module.exports = async s => {
                 let password = s.getMsg();
                 await s.reply('正在登陆中ing');
                 let startTime = Date.now()
-                let { data: res } = await axios.post(luflyApi + '/api/user/login/do', { username, password });
+                let { data: loginRes } = await axios.post(luflyApi + '/api/user/login/do', { username, password });
                 let endTime = Date.now()
 
 
 
-                if (res.code == 0) {
-                    await s.reply(`======JD登录通知======\n登录用户: ${res.data['pin']}\n登录时间: ${getNow()}\n耗时: ${(endTime - startTime) / 1000}s`)
-                    return
+                if (loginRes.code == 0) {
+                    await s.reply(`======JD登录通知======\n登录用户: ${loginRes.data['pin']}\n登录时间: ${getNow()}\n耗时: ${(endTime - startTime) / 1000}s`)
+                    let TOKEN = loginRes.data['token']
+                    let { data: infoRes } = await axios.post(luflyApi + '/api/user/info', { params: "" }, {
+                        headers: {
+                            'token': `${TOKEN}`
+                        }
+                    });
+                    if (infoRes.code == 0) {
+                        if (infoRes.data['notify']['type'] == 'wxpusher') {
+                            if (infoRes.data['notify']['params'] == '') {
+                                let { data: pusherRes } = await axios.post(luflyApi + '/api/user/wxpusher', { params: "" }, {
+                                    headers: {
+                                        'token': `${TOKEN}`
+                                    }
+                                });
+                                if (pusherRes.code == 0) {
+                                    await s.reply({
+                                        type: 'image', // video
+                                        msg: '请用微信扫码绑定通知方式',
+                                        path: pusherRes.data,
+                                    });
+                                    await s.reply('请微信扫码绑定通知方式 后续刷新失败则通知');
+                                    return
+                                }
+                            }
+
+                        }
+                    }
+
 
                 }
-                if (res.code == 1) {
-                    await s.reply('登录风控，请先去该链接验证,验证成功后白屏即可返回，输入ok后系统自动重新登录' + res.data)
+                if (loginRes.code == 1) {
+                    await s.reply('登录风控，请先去该链接验证,验证成功后白屏即可返回，输入ok后系统自动重新登录' + loginRes.data)
                     let risk_input = await s.waitInput(async (s) => {
                         input = s.getMsg();
                         if (input == 'ok') {
                             let startTime = Date.now()
-                            let { data: res } = await axios.post(luflyApi + '/api/user/login/do', { username, password });
+                            let { data: loginRes2 } = await axios.post(luflyApi + '/api/user/login/do', { username, password });
                             let endTime = Date.now()
-                            if (res.code == 0) {
+                            if (loginRes2.code == 0) {
 
-                                await s.reply(`======JD登录通知======\n登录用户: ${res.data['pin']}\n登录时间: ${getNow()}\n耗时: ${(endTime - startTime) / 1000}s`)
-                                return
+                                await s.reply(`======JD登录通知======\n登录用户: ${loginRes.data['pin']}\n登录时间: ${getNow()}\n耗时: ${(endTime - startTime) / 1000}s`)
+                                let TOKEN = loginRes.data['token']
+                                let { data: infoRes } = await axios.post(luflyApi + '/api/user/info', { params: "" }, {
+                                    headers: {
+                                        'token': `${TOKEN}`
+                                    }
+                                });
+                                if (infoRes.code == 0) {
+                                    if (infoRes.data['notify']['type'] == 'wxpusher') {
+                                        if (infoRes.data['notify']['params'] == '') {
+                                            let { data: pusherRes } = await axios.post(luflyApi + '/api/user/wxpusher', { params: "" }, {
+                                                headers: {
+                                                    'token': `${TOKEN}`
+                                                }
+                                            });
+                                            if (pusherRes.code == 0) {
+                                                await s.reply({
+                                                    type: 'image', // video
+                                                    msg: '请用微信扫码绑定通知方式',
+                                                    path: pusherRes.data,
+                                                });
+                                                await s.reply('请微信扫码绑定通知方式 后续刷新失败则通知');
+                                                return
+                                            }
+                                        }
+
+                                    }
+                                }
+
                             }
                             else {
                                 await s.reply('登录失败 请明天再次过验证')
@@ -79,8 +133,8 @@ module.exports = async s => {
                     }, 30);
                     if (risk_input === null) return s.reply('超时退出/已退出');
                 }
-                if (res.code == 2) {
-                    await s.reply('登录失败' + res.msg)
+                if (loginRes.code == 2) {
+                    await s.reply('登录失败' + loginRes.msg)
                     return
                 }
 
